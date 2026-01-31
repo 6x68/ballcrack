f9aeedec1dba4b9c9f7608abf4146bb508f30eed
crackbob <crackbob.999@gmail.com>
2026-01-30 21:42:49 -0500

    better killaura

--- src/Module/modules/combat/Killaura.js
+++ src/Module/modules/combat/Killaura.js
@@ -5,12 +5,15 @@ import Module from "../../Module";
 export default class Killaura extends Module {
     constructor() {
         super("Killaura", "Combat", {
-            "Y Offset": 1.62,
-            "Reach": 10,
-            "Delay": 100
+            "Delay": 100,
+            "Auto Block": true
         });
+
         this.lastExecutionTime = null;
+        this.blocking = false;
     }
+
+    ignoreEntities = ["EntityItem", "EntityXPOrb"]
 
     onTick() {
         const currentTime = Date.now();
@@ -20,13 +23,35 @@ export default class Killaura extends Module {
         }
     }
 
+    block () {
+        hooks.game.controller.sendUseItem(hooks.game.player, hooks.game.world, hooks.game.player.inventory.getCurrentItem());
+        this.blocking = true;
+    }
+
+    unblock () {
+        hooks.game.controller.onStoppedUsingItem(hooks.game.player);
+        this.blocking = false;
+    }
+
     tryKill () {
+        let attacked = false;
+        let autoBlock = this.options["Auto Block"];
+
         hooks.game.world.loadedEntityList.forEach(ent => {
             let dist = mathUtils.distanceBetween(ent.pos, hooks.game.player.pos);
-            if (hooks.game.player.id !== ent.id && dist < 14) {
+            if (hooks.game.player.id !== ent.id && dist < 14 && !this.ignoreEntities.includes(ent.constructor.name)) {
+                attacked = true;
+
+                if (autoBlock) this.unblock();
                 hooks.game.controller.objectMouseOver.hitVec = ent.pos.clone();
                 hooks.game.controller.attackEntity(ent);
+                if (autoBlock) this.block();
+
             }
         })
+
+        if (!attacked) {
+            if (autoBlock) this.unblock();
+        }
     }
 }
